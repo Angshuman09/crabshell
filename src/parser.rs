aae76941bd59d4196ff2ed1a8744ede2f4d5c6cf
Angshuman Kashyap <angshumankashyap69yap69@gmail.com>
2026-03-28 22:59:28 +0530

    implement double quote checking

--- src/parser.rs
+++ src/parser.rs
@@ -1,37 +1,42 @@
+
+
 pub fn tokenize(input: &str) -> Vec<String> {
-    let mut result = Vec::new();
+    let mut args = Vec::new();
     let mut current = String::new();
     let mut in_single_quote = false;
+    let mut in_double_quote = false;
     let mut has_content = false;
 
-    for c in input.chars() {
-        if in_single_quote {
-            if c == '\'' {
-                in_single_quote = false;
-            } else {
-                current.push(c);
+    let mut chars = input.chars().peekable();
+
+    while let Some(c) = chars.next(){
+         match c {
+            '\'' if !in_double_quote =>{
+                in_single_quote = !in_single_quote;
                 has_content = true;
             }
-        } else {
-            if c == '\'' {
-                in_single_quote = true;
+            '"' if !in_single_quote =>{
+                in_double_quote = !in_double_quote;
                 has_content = true;
-            } else if c.is_whitespace() {
-                if has_content {
-                    result.push(current.clone());
+            }
+            c if c.is_whitespace() && !in_single_quote && !in_double_quote =>{
+                if has_content{
+                    args.push(current.clone());
                     current.clear();
-                    has_content = false;
+                    has_content=false;
                 }
-            }else{
+            }
+
+            _ =>{
                 current.push(c);
-                has_content= true;
+                has_content = true;
             }
         }
     }
 
     if has_content{
-        result.push(current);
+        args.push(current);
     }
 
-    result
+    args
 }
